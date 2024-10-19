6be9c84cfb6e458d30bb5b93c2c503c087c98a21
Le Tan Dai <letandai1304@gmail.com>
2024-10-19 16:45:41 +0700

    lab7

---
+++ GridPanel/MainWindow.xaml.cs
@@ -0,0 +1,29 @@
+﻿using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace GridPanel
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+        private void btnDisplay_Click(object sender, RoutedEventArgs e)
+        {
+            string carinfor = $"Car name: {txtCarName.Text}\n" + $"Color : {txtColor.Text} \n Brand: {txtBrand.Text}";
+            MessageBox.Show(carinfor, "Car Details");
+        }
+    }
+}
